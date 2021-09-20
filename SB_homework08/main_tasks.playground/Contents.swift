83592916bb05f3ea50d60c85c40c6c0087dd511a
foggydawning <foggydawning@gmail.com>
2021-09-20 11:25:14 +0300

    Task №1 is ready

--- SB_homework08/main_tasks.playground/Contents.swift
+++ SB_homework08/main_tasks.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+// homework08
+// created by Chub Ilia
+// start date of execution: 20.09.2021
 
-var greeting = "Hello, playground"
+// task №1
+struct Person {
+    var name: String
+    var age: Int
+
+ 
+    func getAgeComparisonString(_ p: Person) -> String {
+        // A function for comparing the age of two Person
+        let variantsDict: [String : String] = [
+                                            "older" : "старше меня",
+                                            "younger": "моложе меня",
+                                            "equal": "такого же возраста, как и я"
+                                            ]
+        var result: String
+        
+        if p.age > self.age{
+            result = variantsDict["older"]!
+        } else if p.age == self.age{
+            result = variantsDict["equal"]!
+        } else{
+            result = variantsDict["younger"]!
+        }
+        
+        return "\(p.name) \(result)"
+    }
+}
